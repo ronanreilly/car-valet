25e98100f79aebd09781a764f53ef5e94c1f5b1a
Ronan Reilly <ronanreilly@icloud.com>
2014-05-23 00:24:56 +0100

    Fixing repository.

---
+++ CarValet/CarValet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarValet
+//
+//  Created by Ronan Sean Reilly on 12/04/2014.
+//  Copyright (c) 2014 Ronan Sean Reilly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RR_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RR_AppDelegate class]));
+    }
+}
